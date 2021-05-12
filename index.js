2694453ae312c4f7b23824ac70af68c3107c9eb6
Emi Yokoo Mantilla <emimantilla@Christians-Air.lan1>
2021-05-11 20:39:19 -0500

    added port

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser"); //by default this app should be assuming request body data as json
 const app = express();
-const port = 4000;
 
 app.use(bodyParser.json());
 app.use(require("./routes/recipes")); //importing the routes module
 
 
 
-
+const PORT = process.env.PORT || 9000
 app.listen(port,() => {
   console.log("Listening to the port", port);
 });
